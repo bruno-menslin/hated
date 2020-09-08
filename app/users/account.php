554b3e3818e4594e1ef35cace0f4147bf645c118
bruno-menslin <bmenslin@gmail.com>
2020-09-08 11:11:09 -0300

    refactor: update account.php structure to apply styles

--- app/users/account.php
+++ app/users/account.php
@@ -1,5 +1,5 @@
 <?php include "/opt/lampp/htdocs/hated/security/authentication/validation.php"; ?>
-<div class="page-account">
+<div id="page-account" class="page">
     <a href="main.php?page=spots/managespots.php">Manage your spots</a>
     <a href="main.php?page=users/upduser.php">Update registration</a>
     <a href="../security/authentication/logout.php">Logout</a>
